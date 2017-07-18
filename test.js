468db2f612da90b36e4cf184d1587b7e677f8ad1
Taskkill <johny95wil@gmail.com>
2017-07-18 22:46:10 +0200

    correct test mistake

--- test.js
+++ test.js
@@ -1,48 +1,15 @@
 //import Iterallise from 'iterallise';
+const Iterallise = require('./main');
 
 // standard generator Example
 
-function Iterallise(gener) {
-  // send gener.toString() into worker
-  // return object
-
-  return {
-    next: (value) => {
-      // value poslat do workeru
-      let __resolve;
-      let __reject;
-
-      worker.onmessage = message => {
-        if (message.data.success) {
-          __resolve(message.data.result);
-        } else {
-          __reject(message.data.error);
-        }
-      }
-
-      worker.postMessage({
-        value: value,
-      });
-
-      return new Promise((resolve, reject) => {
-        __resolve = resolve;
-        __reject = reject;
-      });
-    },
-    done: false // s prvnim donem ve workeru se nastavi i tohle
-  }
-}
-
-
-
-
 function* gener(input) {
   while (true) {
-    yield(input++);
+    yield input++;
   }
 }
 
-let iter = gener(2);
+let iter = gener(4);
 
 console.log(iter.next().value);
 console.log(iter.next().value);
@@ -53,11 +20,7 @@ console.log(iter.next().value);
 // parallel
 // it returns promise not '{done: bool, value: val}'
 
-let it2 = Iterallise(function* gener(input) {
-  while (true) {
-    yield(input++);
-  }
-});
+let it2 = Iterallise(gener, 4);
 
 it2.next().then(res => console.log(res));
 it2.next().then(res => console.log(res));
